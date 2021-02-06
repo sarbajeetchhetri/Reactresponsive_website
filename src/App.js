c01cff76c2c4acc8f25cdeaeee8a3d63cd35defd
Sarbajeet <sarbajeetchhetri36@gmail.com>
2021-02-06 16:34:38 +0545

    Navigation Bar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Home from "./Pages/Home";
 import Services from "./Pages/Services";
 import Contact from "./Pages/Contact";
 import About from "./Pages/About";
+import Navbar from "./Pages/Navbar";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 
@@ -13,8 +16,9 @@ const App = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/services" component={Services} />
         <Route exact path="/contact" component={Contact} />
-        <Route exact path="/About" component={About} />
-        <Redirect to ="/" />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/navbar" component={Navbar} />
+        <Redirect to = "/" />
 
 
       </Switch>
