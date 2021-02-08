30191c09c7b1913d9332627bfe411f2f230d19c3
Sarbajeet <sarbajeetchhetri36@gmail.com>
2021-02-08 09:08:39 +0545

    Added Images folder for photo

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import web from "../Images/grow.png";
 
 const Home = () => {
   return (
@@ -9,7 +10,7 @@ const Home = () => {
             <div className="col-10 mx-auto">
                 <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-2">
                     <h1>
-                        Grow your Business with <strong> Sarbajeet</strong>
+                        Grow your Business with <strong className="brand-name"> Sarbajeet</strong>
                     </h1>
                     <h2 className="my-3">
                     We are the team of talented developer making websites
@@ -18,6 +19,10 @@ const Home = () => {
                     <div className="mt-3">
                     <a href="" className="btn-getstarted"> Get Started</a>
                     </div>
+                </div>
+                <div className="col-lg-6 order-1 order-lg-2 header-image">  
+                <img src={web} className="image-fluid animated" alt="home image" />
+
                 </div>
             </div>
           </div>
