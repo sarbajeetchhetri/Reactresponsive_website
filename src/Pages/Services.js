a623116254f499b002b5c2fbc3a93b583b8a58bf
Sarbajeet <sarbajeetchhetri36@gmail.com>
2021-02-22 13:41:14 +0545

    Modification of Service Page

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Services=()=>{
-    return (
-        <>
-            <h1>This is  a Services Page </h1>
-        </>
-    )
-}
+const Services = () => {
+  return (
+    <>
+      <div className="my-5">
+        <h1 className="tab-center">Our Services</h1>
+        </div>
+        <div className="container-fluid nav-bg mb-5">
+          <div className="row">
+            <div className="col-10 mx-auto">
+                <div className="row">
+                    <div className="col-md-4 col-10 mx-auto">
+
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+   
+    </>
+  );
+};
 
 export default Services;
