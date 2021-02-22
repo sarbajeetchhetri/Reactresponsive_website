7844d8eecc4eb7e81e96deee3cf7b091b3dfe25e
Sarbajeet <sarbajeetchhetri36@gmail.com>
2021-02-22 17:36:56 +0545

    Passing the Json data in Servdata componenet

---
+++ src/Pages/Servdata.js
@@ -0,0 +1,39 @@
+import andriod from "../Images/android.png"
+import digitalmarketing from "../Images/digitalm.png"
+import webdev from "../Images/web.jpg"
+import python from "../Images/python.png"
+import software from "../Images/soft.jpg"
+import webdesigner from "../Images/webdesigner.jpg"
+
+
+const Servdata =[
+{
+    imgsrc: andriod,
+    title: "Android development",
+},
+
+{
+    imgsrc: digitalmarketing,
+    title: "Digital Marketing",
+},
+
+{
+    imgsrc: webdev,
+    title: "Web Development",
+},
+{
+    imgsrc: python,
+    title: "Python",
+},
+{
+    imgsrc: software,
+    title: "Software Development",
+},
+{
+    imgsrc: webdesigner,
+    title: "Web Designer",
+},
+
+]
+
+export default Servdata;
