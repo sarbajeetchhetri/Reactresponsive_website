d8af2bb1150826386c4f3c946557727b11a2eb8c
Sarbajeet <sarbajeetchhetri36@gmail.com>
2021-02-23 10:37:31 +0545

    added contact pages

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,11 +1,112 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Contact=()=>{
-    return (
-        <>
-            <h1>This is  a Contact Page </h1>
-        </>
-    )
-}
+const Contact = () => {
+  const [data, setData] = useState({
+    fullname: "",
+    phone: "",
+    email: "",
+    message: "",
+  });
+
+  const Inputevent = (event) => {
+    const { name, value } = event.target;
+    setData((preVal) => {
+      return {
+        ...preVal,
+        [name]: value,
+      };
+    });
+  };
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    alert(
+      ` My name is ${data.fullname}. 
+          My mobile number is ${data.phone}
+          My email address is ${data.email}
+          I want to say ${data.message}   `
+    );
+  };
+
+  return (
+    <>
+      <div className="my-5">
+        <h1 className="text-center">Contact us</h1>
+      </div>
+
+      <div className="container contact-div">
+        <div className="row">
+          <div className="col-md-6 col-10 mx-auto">
+            <form onSubmit={formSubmit}>
+              <div className="mb-3">
+                <label for="exampleFormControlInput1" className="form-label">
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="fname"
+                  placeholder="Enter your name"
+                  name="fullname"
+                  value={data.fullname}
+                  onChange={Inputevent}
+                />
+              </div>
+
+              <div className="mb-3">
+                <label for="exampleFormControlInput1" className="form-label">
+                  Phone Number
+                </label>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="phone"
+                  placeholder="Enter your mobile number"
+                  name="phone"
+                  value={data.phone}
+                  onChange={Inputevent}
+                />
+              </div>
+
+              <div className="mb-3">
+                <label for="exampleFormControlInput1" className="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="name@example.com"
+                  name="email"
+                  value={data.email}
+                  onChange={Inputevent}
+                />
+              </div>
+              <div className="mb-3">
+                <label for="exampleFormControlTextarea1" className="form-label">
+                  Message
+                </label>
+                <textarea
+                  className="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                  name="message"
+                  value={data.message}
+                  onChange={Inputevent}
+                ></textarea>
+              </div>
+
+              <div className="col-12">
+                <button className="btn btn-outline-primary" type="submit">
+                  Submit form
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Contact;
--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,23 +1,36 @@
 import React from "react";
+import Servdata from "./Servdata";
+import Card from "./Card";
+
 
 const Services = () => {
   return (
     <>
       <div className="my-5">
-        <h1 className="tab-center">Our Services</h1>
-        </div>
-        <div className="container-fluid nav-bg mb-5">
-          <div className="row">
-            <div className="col-10 mx-auto">
-                <div className="row">
-                    <div className="col-md-4 col-10 mx-auto">
+        <h1 className="text-center">Our Services</h1>
+      </div>
+      <div className="container-fluid  mb-5">
+        <div className="row">
+          <div className="col-10 mx-auto ">
+            <div className="row gy-4">
 
-                    </div>
-                </div>
+        {
+          Servdata.map((val, index) => {
+            return<Card
+              key={index}
+              imgsrc={val.imgsrc}
+              title={val.title}
+            />
+          })
+
+        }
+
+            
+
             </div>
           </div>
         </div>
-   
+      </div>
     </>
   );
 };
